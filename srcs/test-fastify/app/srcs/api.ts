d089b5503f75bfaddb355bb882d5e38f89d2ad8a
Gaston Lions <glions@f6r7s9.clusters.42paris.fr>
2025-06-16 14:42:02 +0200

    ajout de route

--- srcs/test-fastify/app/srcs/api.ts
+++ srcs/test-fastify/app/srcs/api.ts
@@ -1,15 +1,34 @@
 import fastify from 'fastify';
 
-const server = fastify();
+const server = fastify({logger: true});
 
 server.get('/ping', async (request, reply) => {
-  return 'pong\n';
+  return {value: 'pong'};
 });
 
-server.listen({ port: 3000, host: '0.0.0.0' }, (err, address) => {
-  if (err) {
-    console.error(err);
+server.get('/users', async (request, reply) => {
+  reply.send([{id:1, name: 'Gaston'}]);
+});
+
+server.get('/users/:id', async (request, reply) => {
+  const { id } = request.params;
+  reply.send([{id:1, name: `Gaston ${id}` }]);
+});
+
+server.post('/users', (request, reply) => {
+  const newUser = request.body;
+  reply.code(201).send( {id: 2, ...newUser });
+})
+
+const start = async () => {
+  try {
+    await server.listen({ port: 3000, host: '0.0.0.0' });
+    console.log(`Server listening at localhost:3000`);
+  } catch (err)
+  {
+    server.log.error(err);
     process.exit(1);
   }
-  console.log(`Server listening at ${address}`);
-});
+};
+
+start();
