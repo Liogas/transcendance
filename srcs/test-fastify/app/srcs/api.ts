e2d66f177853429be09bbf32785f44d7ca9ba47b
Gaston_Lions <gastonlions.pro@gmail.com>
2025-06-16 20:48:40 +0200

    type générique + JSON Shema example

--- srcs/test-fastify/app/srcs/api.ts
+++ srcs/test-fastify/app/srcs/api.ts
@@ -1,24 +1,64 @@
 import fastify from 'fastify';
 
+
+interface IQueryString
+{
+  'name': string;
+  'pwd': string;
+};
+
+interface IHeaders
+{
+  'h-Custom': string;
+}
+
+interface IReply
+{
+  200: { success: boolean };
+  302: { url: string };
+  '4xx': { error: string };
+}
+
 const server = fastify({logger: true});
 
-server.get('/ping', async (request, reply) => {
-  return {value: 'pong'};
+server.post<{
+  Querystring: IQueryString,
+  Headers: IHeaders,
+  Reply: IReply
+}>('/register', {
+  schema: {
+    body: {
+      type: 'object',
+      required: ['name', 'pwd'],
+      properties: {
+        name: { type: 'string' },
+        pwd: { type: 'string' }
+      }
+    },
+    response: {
+      '2xx': {
+        type: 'object',
+        properties: { success: { type: 'boolean' } }
+      },
+      '4xx': {
+        type: 'object',
+        properties: { error: { type: 'string' } }
+      },
+      302: {
+        type: 'object',
+        properties: { error: { type: 'string' } }
+      }
+    }
+  }
+}, async (request, reply) => {
+  const {name, pwd} = request.query;
+  const customHeader = request.headers['h-Custom'];
+  console.log(name, pwd, customHeader);
+  reply.code(200).send({success: true});
+  // reply.code(200).send("ZzZ");
+  // reply.code(404).send({error: 'Not found'});
+  // return `logged in!`;
 });
-
-server.get('/users', async (request, reply) => {
-  reply.send([{id:1, name: 'Gaston'}]);
-});
-
-server.get('/users/:id', async (request, reply) => {
-  const { id } = request.params;
-  reply.send([{id:1, name: `Gaston ${id}` }]);
-});
-
-server.post('/users', (request, reply) => {
-  const newUser = request.body;
-  reply.code(201).send( {id: 2, ...newUser });
-})
 
 const start = async () => {
   try {
